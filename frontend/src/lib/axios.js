074d7e2035a0c0d6fc181035a767a1d4e2c77b80
trnqtonn <trantoan121406@gmail.com>
2026-09-08 14:35:14 +0700

    chuẩn bị deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"; // Thay đổi URL cơ sở của API dựa trên môi trường
+
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
 }); // Thay đổi URL cơ sở của API
 
 export default api;
